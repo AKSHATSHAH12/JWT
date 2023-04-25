4469b92e7c328d18638c32291f638a4f67225872
akshat <project.94284@gmail.com>
2023-04-25 09:58:25 +0530

    JWT

---
+++ db.js
@@ -0,0 +1,40 @@
+const users = [{
+    email:"akshat@gmail.com",
+    password:"abcdefg"
+}];
+
+const publicPosts = [
+    {
+        title: "Free Tips",
+        content: "these are some tips"
+    },
+    {
+        title: "Free Tips",
+        content: "these are some tips"
+    },
+    {
+        title: "Free Tips",
+        content: "these are some tips"
+    },
+]
+
+const privatePosts = [
+    {
+        title: "paid Tips",
+        content: "these are some tips"
+    },
+    {
+        title: "paid Tips",
+        content: "these are some tips"
+    },
+    {
+        title: "paid Tips",
+        content: "these are some tips"
+    },
+]
+
+
+module.exports = { users,
+    publicPosts,privatePosts
+
+}
